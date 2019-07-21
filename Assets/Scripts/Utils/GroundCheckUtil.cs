bee8b07abe3691f8f0d3bef7a2d90d7033094f0d
TheSemo <maleficarchyal@gmail.com>
2019-07-21 11:23:11 +0200

    extended the debug ray for ground checks to make it more visible

--- Assets/Scripts/Utils/GroundCheckUtil.cs
+++ Assets/Scripts/Utils/GroundCheckUtil.cs
@@ -10,7 +10,7 @@ public static class GroundCheckUtil
             float distanceToGround = characterBounds.extents.y;
             Vector2 rayStart = new Vector2(characterView.transform.position.x,
                 characterView.transform.position.y - distanceToGround - 0.02f);
-            Debug.DrawRay(rayStart, Vector2.down * 0.02f, Color.red, 3f);
+            Debug.DrawRay(rayStart, Vector2.down * 1, Color.red, 3f);
             RaycastHit2D hit = Physics2D.BoxCast(rayStart, new Vector2(characterBounds.size.x - 0.1f, 0.02f), 0f,
                 Vector2.down);
             if (hit.collider != null)
